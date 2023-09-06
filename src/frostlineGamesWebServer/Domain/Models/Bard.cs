f21abf56aca97e1816e2354f659ea1600593be53
Rezan-Soylemez <rezan.sm@gmail.com>
2023-09-06 14:46:13 +0300

    BugFixed - LoginAndRegisteruserCanBeRegisterAndLogin

--- src/frostlineGamesWebServer/Domain/Models/Bard.cs
+++ src/frostlineGamesWebServer/Domain/Models/Bard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Security.Entities;
+using System;
 using System.Collections.Generic;
 
 namespace Persistence.Models;
--- src/frostlineGamesWebServer/Domain/Models/UserDetailImageFile.cs
+++ src/frostlineGamesWebServer/Domain/Models/UserDetailImageFile.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Domain.Entities.Users;
 
 namespace Persistence.Models;
 
